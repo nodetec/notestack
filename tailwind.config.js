578a5d44fb31310ecbf55b079a4f6dcf42def70a
Christian Chiarulli <chris.machine@pm.me>
2023-01-27 00:51:12 -0500

    revert nextjs to 13.1.1 since any higher breaks routes for some reason

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
