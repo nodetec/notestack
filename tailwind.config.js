e24bf97240731d06d7eec51e5cc3b72577c2efce
mahcodes <mhmdali102@bk.ru>
2023-01-27 06:39:13 +0200

    fix: profile menu position

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         "red-hover": "rgb(210, 19, 7)",
         gray: "rgb(117, 117, 117)",
         "gray-hover": "rgb(41, 41, 41)",
+        "light-gray": "rgb(200, 200, 200)",
       },
       boxShadow: {
         "profile-menu": "rgb(230, 230, 230) 0px 1px 4px",
