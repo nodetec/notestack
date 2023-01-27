aec08e76a4e800f483667e8da0559d5b4b88ce0b
mahcodes <mhmdali102@bk.ru>
2023-01-27 04:25:35 +0200

    feat: use roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      "main": ["var(--main-font)", "sans-serif"],
+    },
     extend: {
       colors: {
         red: "rgb(247, 0, 0)",
