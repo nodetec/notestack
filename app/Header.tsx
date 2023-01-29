473e4037af860f662b1f0dd3dbbd66b8951f612c
Christian Chiarulli <chris.machine@pm.me>
2023-01-29 04:42:55 -0500

    feat: add search input for header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,6 @@
 import Login from "./Login";
 import Logo from "./Logo";
+import Search from "./Search";
 import WriteButton from "./WriteButton";
 
 const Header = () => {
@@ -7,7 +8,10 @@ const Header = () => {
     <header className="bg-white z-30 sticky top-0 w-full left-0 right-0 border-b border-b-light-gray px-4 py-3">
       <nav>
         <div className="flex items-center justify-between w-full gap-4 flex-col sm:flex-row">
-          <Logo />
+          <div className="flex flex-row items-center justify-start gap-6">
+            <Logo />
+            <Search />
+          </div>
           <div className="flex gap-4 items-center">
             <WriteButton />
             <Login />
---
+++ app/Search.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-row items-center gap-2 pl-4 py-2 bg-neutral-100 rounded-full">
+      <BsSearch className="text-neutral-500" size="15" />
+      <input
+        placeholder="this doesn't work lol"
+        className="bg-neutral-100 outline-none"
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Search;
