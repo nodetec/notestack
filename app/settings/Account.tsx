04637d63aa9f5282f6b1b7e35ef802e44e65a432
mahcodes <mhmdali102@bk.ru>
2023-01-31 06:22:27 +0200

    feat: account item init

--- app/settings/Account.tsx
+++ app/settings/Account.tsx
@@ -1,6 +1,45 @@
 "use client";
+
+import { useState } from "react";
+
 const Account = () => {
-  return <h1>account settings</h1>;
+  const [profile, setProfile] = useState({
+    newName: "test",
+    newNip05: "",
+    newNpub: "",
+    newAbout: "",
+    newPicture: "",
+    newProfilePubkey: "",
+    newLoggedInContactList: "",
+    newLoggedInPubkey: "",
+    newLud06: "",
+    newLud16: "",
+  });
+  const {
+    newName,
+    newNip05,
+    newNpub,
+    newAbout,
+    newPicture,
+    newProfilePubkey,
+    newLoggedInContactList,
+    newLoggedInPubkey,
+    newLud06,
+    newLud16,
+  } = profile;
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Item title="Name" value={newName} />
+    </div>
+  );
 };
+
+const Item = ({ title, value }: { title: string; value: string }) => (
+  <button className="w-full flex items-center gap-2 justify-between text-sm">
+    <h3>{title}</h3>
+    <span className="text-gray hover:text-gray-hover">{value}</span>
+  </button>
+);
 
 export default Account;
