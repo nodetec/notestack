5f863b8d10c2a916d2ae1e9a156efaf63375585d
mahcodes <mhmdali102@bk.ru>
2023-01-31 02:03:36 +0200

    refactor: tooltip to re-use for search

--- app/u/[npubkey]/page.tsx
+++ app/u/[npubkey]/page.tsx
@@ -25,6 +25,7 @@ export default function ProfilePage() {
   });
   const TABS = ["Home", "About"];
   const [activeTab, setActiveTab] = useState<(typeof TABS)[number]>(TABS[0]);
+  const [showTooltip, setShowTooltip] = useState(false);
   const { copyToClipboard, isCopied, isError } = useCopy();
   const { setNotifyMessage } = useContext(NotifyContext);
   const pathname = usePathname();
@@ -58,7 +59,8 @@ export default function ProfilePage() {
             </h1>
             <Tooltip
               direction="bottom"
-              showOn="click"
+              show={showTooltip}
+              toggle={() => setShowTooltip((current) => !current)}
               Component={
                 <Button
                   color="transparent"
@@ -73,16 +75,21 @@ export default function ProfilePage() {
                   variant="ghost"
                   color="transparent"
                   size="xs"
-                  onClick={() => copyToClipboard(npub)}
+                  onClick={() => {
+                    copyToClipboard(npub);
+                    setShowTooltip(false);
+                  }}
                 >
                   Copy link to profile
                 </Button>
                 <Button
                   color="transparent"
+                  variant="ghost"
                   size="xs"
-                  onClick={() =>
-                    setNotifyMessage("click the ⚡ button under the user card")
-                  }
+                  onClick={() => {
+                    setNotifyMessage("click the ⚡ button under the user card");
+                    setShowTooltip(false);
+                  }}
                 >
                   Support this author
                 </Button>
