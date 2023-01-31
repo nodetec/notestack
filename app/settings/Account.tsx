79b3c11924055fa4df13b83b40428ad3de25277c
mahcodes <mhmdali102@bk.ru>
2023-01-31 05:11:22 +0200

    feat: settings tabs

---
+++ app/settings/Account.tsx
@@ -0,0 +1,6 @@
+"use client";
+const Account = () => {
+  return <h1>account settings</h1>;
+};
+
+export default Account;
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import Aside from "../Aside";
 import Content from "../Content";
 import Main from "../Main";
 import RecommendedEvents from "../RecommendedEvents";
+import Tabs from "../Tabs";
+import Account from "./Account";
+import Notifications from "./Notifications";
+import Publishing from "./Publishing";
 
 const ARTICLES = [
   "616c252e86c5488faf65b5247800b517f00c658b528435bde12c481c4c0b3f37",
@@ -12,9 +17,20 @@ const ARTICLES = [
 ];
 
 const SettingsPage = () => {
+  const TABS = ["Account", "Publishing", "Notifications"];
+  const [activeTab, setActiveTab] = useState<(typeof TABS)[number]>(TABS[0]);
+
   return (
     <Main>
-      <Content></Content>
+      <Content>
+        <h1 className="text-5xl font-medium my-12">Settings</h1>
+        <Tabs TABS={TABS} activeTab={activeTab} setActiveTab={setActiveTab} />
+        <div className="mt-12">
+          {activeTab === "Account" && <Account />}
+          {activeTab === "Publishing" && <Publishing />}
+          {activeTab === "Notifications" && <Notifications />}
+        </div>
+      </Content>
       <Aside>
         <RecommendedEvents title="Suggested help articles" EVENTS={ARTICLES} />
       </Aside>
