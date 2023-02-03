51a167262f6dce622f060f467f7073d0c4e1ce21
mahcodes <mhmdali102@bk.ru>
2023-02-03 06:25:25 +0200

    fix: i'm stupid

--- app/tag/[tagname]/page.tsx
+++ app/tag/[tagname]/page.tsx
@@ -77,6 +77,7 @@ export default function TagPage() {
           ]}
         />
         <Topics
+          title="Recommended Topics"
           TOPICS={[
             "nostr",
             "lightning",
