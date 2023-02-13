83ebdf519c7707f63186dfc738720f87eab19565
Christian Chiarulli <chris.machine@pm.me>
2023-02-13 02:14:21 -0500

    bug: wellorder and snort cause high sustained CPU utilization for some reason when fetching initial events

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,7 +1,7 @@
 export const RELAYS = [
-  "wss://relay.snort.social",
   "wss://relay.damus.io",
-  "wss://nostr-pub.wellorder.net",
+  // "wss://relay.snort.social",
+  // "wss://nostr-pub.wellorder.net",
   "wss://nostr.nostrelay.org",
   // "wss://relay.nostr.ch",
   // "wss://nostr.bitcoiner.social",
