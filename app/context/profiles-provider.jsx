29814ed16a258a4710bcc8f0708bb474c10436e6
Christian Chiarulli <chris.machine@pm.me>
2023-02-13 01:32:42 -0500

    refactor: use subscribe function for profiles-provider

--- app/context/profiles-provider.jsx
+++ app/context/profiles-provider.jsx
@@ -8,59 +8,25 @@ export const ProfilesContext = createContext([]);
 export default function ProfilesProvider({ children }) {
   const [profiles, setProfiles] = useState({});
   const [reload, setReload] = useState(false);
-  const { relayUrl, connect } = useContext(RelayContext);
+  const { relayUrl, subscribe } = useContext(RelayContext);
 
   const addProfiles = async (pubkeys) => {
     if (!relayUrl) return;
 
-    const relay = await connect(relayUrl);
-    if (!relay) return;
-
-    // console.log("Looking up profiles:", pubkeys);
-    // console.log("allPubkeys:", allPubkeys);
-
-
-    // const filteredEvents = eventArray.filter((e1: Event, index: number) => {
-    //   if (e1.content === "") {
-    //     return false;
-    //   }
-    //   return eventArray.findIndex((e2: Event) => e2.id === e1.id) === index;
-    // });
-
-    // const unseenPubkeys = pubkeys.filter((pubkey) => {
-    //   if (allPubkeys.includes(pubkey)) {
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-
-    // })
-    // let pubkeysSet = new Set([...allPubkeys, ...pubkeys]);
-
-
-    // setAllPubkeys(Array.from(pubkeysSet));
-
-    // if (unseenPubkeys.length === 0) {
-    //   setReload(!reload);
-    // }
-
-    // console.log("filtered pubkeys:", unseenPubkeys);
-
     let relayName = relayUrl.replace("wss://", "");
-    let sub = relay.sub([
-      {
-        kinds: [0],
-        authors: pubkeys,
-      },
-    ]);
+
+    const filter = {
+      kinds: [0],
+      authors: pubkeys,
+    };
+
     let events = [];
-    sub.on("event", (event) => {
-      // console.log("Looking up profile EVENT:", event.pubkey);
-      // @ts-ignore
+
+    const onEvent = (event) => {
       events.push(event);
-    });
-    sub.on("eose", () => {
-      sub.unsub();
+    };
+
+    const onEOSE = () => {
       if (events.length !== 0) {
         events.forEach((event) => {
           let profileKey = `profile_${relayName}_${event.pubkey}`;
@@ -70,9 +36,9 @@ export default function ProfilesProvider({ children }) {
           setReload(!reload);
         });
       }
-      // console.log("profiles:", profiles);
-      // console.log("DONE Looking up profiles EVENT:", events);
-    });
+    };
+
+    subscribe([relayUrl], filter, onEvent, onEOSE);
   };
 
   return (
