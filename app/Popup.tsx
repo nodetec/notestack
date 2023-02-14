52cb53e28dcb8cf22ca87f07b146d80e0cc574ad
mahcodes <mhmdali102@bk.ru>
2023-02-14 23:52:05 +0200

    fix: resolve conflicts, in fact re-wrote it

--- app/Popup.tsx
+++ app/Popup.tsx
@@ -31,35 +31,37 @@ const Popup = ({
     };
   }, [setIsOpen]);
 
-  if (!isOpen){
-    if(typeof document !== "undefined"){
-      document.getElementById('blogstack-html')?.classList.remove("overflow-y-hidden");
+  if (!isOpen) {
+    if (typeof document !== "undefined") {
+      document.body.className = "font-main";
     }
     return null;
   }
-  if(typeof document !== "undefined"){
-    document.getElementById('blogstack-html')?.classList.add("overflow-y-hidden");
+  if (typeof document !== "undefined") {
+    document.body.className = "overflow-hidden font-main";
   }
 
   return (
     <Fragment>
       <div
-        className={`z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[32rem] border-2 bg-white/0 rounded-md ${className}`}
+        className={`z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[32rem] bg-white border border-light-gray shadow-popup text-gray-hover rounded-md ${className}`}
       >
-        <Button
-          icon={<IoMdClose size={24} />}
-          color="transparent"
-          variant="ghost"
-          className="absolute w-fit right-0 top-0 opacity-70 hover:opacity-100"
-          onClick={() => setIsOpen(false)}
-        />
-        <div className="flex flex-col justify-center items-stretch gap-4 p-10">
-          <h3 className="text-xl text-center pb-4">{title}</h3>
+        <div className="flex flex-col justify-center items-stretch gap-4 px-10 py-10 pt-8">
+          <div className="flex justify-between gap-4">
+            <h3 className="text-xl font-bold pb-4">{title}</h3>
+            <Button
+              icon={<IoMdClose size={20} />}
+              color="transparent"
+              variant="ghost"
+              className="w-fit translate-x-1/2 -translate-y-1/2 opacity-70 hover:opacity-100"
+              onClick={() => setIsOpen(false)}
+            />
+          </div>
           {children}
         </div>
       </div>
       <div
-        className="z-40 fixed top-0 left-0 w-full h-full bg-white/[.97]"
+        className="z-40 fixed top-0 left-0 w-full h-full bg-black/20"
         onClick={() => setIsOpen(false)}
       />
     </Fragment>
