e5e1b10373a1ac07da20cbf8921e084164b8ec5a
mahcodes <mhmdali102@bk.ru>
2023-02-21 22:17:24 +0200

    feat: loading ui

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import AnimatedLogo from "./AnimatedLogo";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <AnimatedLogo size={50} />
+    </div>
+  );
+};
+
+export default Loading;
