4a32d63e642cf4080b124fe7c05b6fd634077974
Christian Chiarulli <chris.machine@pm.me>
2023-02-23 02:46:27 -0500

    test

--- app/u/[npubkey]/page.tsx
+++ app/u/[npubkey]/page.tsx
@@ -1,37 +1,7 @@
 "use client";
-
-import About from "@/app/About";
-import Aside from "@/app/Aside";
-import BlogFeed from "@/app/BlogFeed";
-import Content from "@/app/Content";
-import type { Event } from "nostr-tools";
 import Main from "@/app/Main";
-import Tabs from "@/app/Tabs";
-import { usePathname } from "next/navigation";
-import { nip19 } from "nostr-tools";
-import { useContext, useEffect, useState } from "react";
-import AuthorTooltip from "@/app/AuthorTooltip";
-import { NostrService } from "@/app/lib/nostr";
-import { RelayContext } from "@/app/context/relay-provider";
-import FollowedRelays from "@/app/FollowedRelays";
-import { FeedContext } from "@/app/context/feed-provider";
-import { ProfilesContext } from "@/app/context/profiles-provider";
-import UserCard from "@/app/components/profile/UserCard";
-import Following from "@/app/components/profile/Following";
-import { DUMMY_PROFILE_API } from "@/app/lib/constants";
 
 export default function ProfilePage() {
-  const TABS = ["Home", "About"];
-  const [activeTab, setActiveTab] = useState<(typeof TABS)[number]>(TABS[0]);
-  const pathname = usePathname();
-  const [events, setEvents] = useState<Event[]>([]);
-  const [isEventsLoading, setIsEventsLoading] = useState(true);
-  // @ts-ignore
-  const { feed, setFeed } = useContext(FeedContext);
-  // @ts-ignore
-  const { addProfiles, profiles, reload } = useContext(ProfilesContext);
-  // const { profile } = useContext(ProfileContext);
-  const { relayUrl, subscribe } = useContext(RelayContext);
 
   // const [name, setName] = useState<string>();
   // const [about, setAbout] = useState<string>("");
@@ -81,24 +51,6 @@ export default function ProfilePage() {
 
   //   subscribe([relayUrl], filter, onEvent, onEOSE);
   // };
-
-  const getProfile = () => {
-    let relayName = relayUrl.replace("wss://", "");
-    const profileKey = `profile_${relayName}_${profilePubkey}`;
-    const profile = profiles[profileKey];
-    if (!profile) {
-      addProfiles([profilePubkey]);
-    }
-    if (profile && profile.content) {
-      const profileContent = JSON.parse(profile.content);
-      setProfile(profileContent);
-      if (!profileContent.picture || profileContent.picture === "") {
-        setPicture(DUMMY_PROFILE_API(npub));
-      } else {
-        setPicture(profileContent.picture);
-      }
-    }
-  };
 
   // look up blogs
   // look up profile
