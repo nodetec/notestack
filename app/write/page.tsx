2bb73d2f52debe0eb1edf344a5849c9e67641068
Christian Chiarulli <chris.machine@pm.me>
2023-02-23 02:35:58 -0500

    test

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,51 +1,7 @@
 "use client";
-import MdEditor from "react-markdown-editor-lite";
-import { useContext, useEffect } from "react";
-import { BlogContext } from "../context/blog-provider";
-import "react-markdown-editor-lite/lib/index.css";
 
 const WritePage = () => {
-  // @ts-ignore
-  const { blog, setBlog } = useContext(BlogContext);
-
-  function setupMarkdown(content: string) {
-    var md = require("markdown-it")();
-    var result = md.render(content || "");
-    return result;
-  }
-
-  // useEffect(() => {
-  //   return () => {
-  //     setBlog({
-  //       title: "",
-  //       summary: null,
-  //       content: "",
-  //       image: null,
-  //       identifier: null,
-  //       publishedAt: null,
-  //       titleValid: true,
-  //       contentValid: true,
-  //     });
-  //   };
-  // }, [setBlog]);
-
-  const handleTitleChange = (evn: any) => {
-    setBlog({ ...blog, title: evn.target.value, titleValid: true });
-  };
-
-  const handleContentChange = (evn: any) => {
-    setBlog({ ...blog, content: evn.text, contentValid: true });
-  };
-
-  return (
-    <input
-      className="w-full p-4 text-lg font-bold focus:outline-none"
-      placeholder="Title..."
-      style={{ backgroundColor: "#f5f5f5" }}
-      value={blog.title}
-      onChange={handleTitleChange}
-    />
-  );
+  return <div>hi</div>;
 };
 
 export default WritePage;
