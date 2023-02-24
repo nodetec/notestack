e9cfe2e688867c518215e686de5dcfef4fd1c5ef
Christian Chiarulli <chris.machine@pm.me>
2023-02-23 20:35:39 -0500

    feat: show relay favicons  (#111)

--- app/FollowedRelays.tsx
+++ app/FollowedRelays.tsx
@@ -4,7 +4,8 @@ import { useContext, useEffect, useState } from "react";
 import { ProfilesContext } from "./context/profiles-provider.jsx";
 
 export default function FollowedRelays() {
-  const { setRelayUrl, activeRelay, allRelays, connect } = useContext(RelayContext);
+  const { setRelayUrl, activeRelay, allRelays, connect } =
+    useContext(RelayContext);
   // @ts-ignore
   const { reload, setReload } = useContext(ProfilesContext);
   const [relayNames, setRelayNames] = useState<string[]>([]);
@@ -31,17 +32,23 @@ export default function FollowedRelays() {
       {relayNames && (
         <div className="flex whitespace-nowrap scrollable-element flex-row gap-2 overflow-x-scroll">
           {relayNames.map((relay: string) => {
+            const relayName = relay.replace("relay.", "");
             return (
               <button
                 key={relay}
                 onClick={() => handleRelayClick(relay)}
                 className={
                   activeRelay && activeRelay.url === "wss://" + relay
-                    ? "border border-black bg-black text-white text-xs sm:text-base rounded-full px-4 py-2"
-                    : "border border-black rounded-full text-xs sm:text-base px-4 py-2"
+                    ? "w-full flex flex-row gap-2 box-border border border-black bg-black text-white text-xs justify-center items-center sm:text-base rounded-full px-7 py-2"
+                    : "w-full flex flex-row gap-2 box-border border border-black rounded-full text-xs justify-center items-center sm:text-base px-7 py-2"
                 }
               >
-                {relay}
+                  <img
+                    className="rounded-full h-6"
+                    src={`https://${relayName}/favicon.ico`}
+                    alt={""}
+                  />
+                  <span>{relay}</span>
               </button>
             );
           })}
