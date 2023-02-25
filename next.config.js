ad13a4f5f95b8eb14ba8b3832661dfdfda1c0f87
Christian Chiarulli <chris.machine@pm.me>
2023-02-24 22:12:21 -0500

    update: nextjs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-const removeImports = require("next-remove-imports")();
+// const removeImports = require("next-remove-imports")();
 
-module.exports = removeImports({
-  reactStrictMode: false,
+// module.exports = removeImports(
+//   {
+//   reactStrictMode: false,
+//   experimental: {
+//     esmExternals: "loose",
+//     appDir: true,
+//     fontLoaders: [
+//       { loader: "@next/font/google", options: { subsets: ["latin"] } },
+//     ],
+//   },
+// });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
-    esmExternals: "loose",
     appDir: true,
+    esmExternals: "loose",
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
   },
-});
+}
+
+module.exports = nextConfig
