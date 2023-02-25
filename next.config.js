364e0d22afef0fd16ae5211c9b93ee4a852a782e
Christian Chiarulli <chris.machine@pm.me>
2023-02-24 22:18:12 -0500

    remove commented code

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-// const removeImports = require("next-remove-imports")();
-
-// module.exports = removeImports(
-//   {
-//   reactStrictMode: false,
-//   experimental: {
-//     esmExternals: "loose",
-//     appDir: true,
-//     fontLoaders: [
-//       { loader: "@next/font/google", options: { subsets: ["latin"] } },
-//     ],
-//   },
-// });
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   experimental: {
     appDir: true,
     esmExternals: "loose",
