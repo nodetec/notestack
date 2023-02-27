e8c46689e6b3904c1774f288e1696e7745409383
mahcodes <mhmdali102@bk.ru>
2023-02-28 01:44:06 +0200

    feat(settings): add dark theme setting

--- app/settings/Appearance.tsx
+++ app/settings/Appearance.tsx
@@ -1,19 +1,30 @@
 "use client";
 
-import { Fragment } from "react";
+import { useEffect } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Switch from "../Switch";
 import Item from "./Item";
 
 const Appearance = () => {
   const [zenMode, setZenMode] = useLocalStorage("zenMode", false);
+  const [darkTheme, setDarkTheme] = useLocalStorage("darkTheme", false);
+
+  useEffect(() => {
+    document.body.dataset.colorMode = darkTheme ? "dark" : "light";
+  }, [darkTheme]);
 
   return (
-    <Fragment>
+    <div className="flex flex-col gap-6">
       <Item title="Zen mode" onClick={() => setZenMode((current) => !current)}>
         <Switch checked={zenMode} />
       </Item>
-    </Fragment>
+      <Item
+        title="Dark theme"
+        onClick={() => setDarkTheme((current) => !current)}
+      >
+        <Switch checked={darkTheme} />
+      </Item>
+    </div>
   );
 };
 
