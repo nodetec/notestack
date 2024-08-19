db382e7b40456014b92d669074a1deb1840a0373
Christian Chiarulli <chris.machine@pm.me>
2024-08-19 05:02:15 -0400

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "warn",
