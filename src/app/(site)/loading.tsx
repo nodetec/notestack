e2aa4d455a663acc6ae772bcab8d430360c35cf6
Christian Chiarulli <chris.machine@pm.me>
2024-08-19 16:20:33 -0400

    add another loading component

---
+++ src/app/(site)/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <main className="min-w-3xl mx-auto mt-12 flex w-full max-w-3xl flex-col items-center gap-y-8">
+      loading...
+    </main>
+  );
+}
