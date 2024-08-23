3b222b095964c3c9289aae5a4808015780350f06
Christian Chiarulli <chris.machine@pm.me>
2024-08-22 23:44:45 -0400

    update article skeleton

--- src/features/article/components/SkeletonArticle.tsx
+++ src/features/article/components/SkeletonArticle.tsx
@@ -2,28 +2,35 @@ import { Skeleton } from "~/components/ui/skeleton";
 
 export async function SkeletonArticle() {
   return (
-    <article className="flex flex-col mx-auto gap-y-4 px-1 max-w-2xl">
-        <Skeleton className="h-10 w-3/4 mb-4" />
+    <>
+      <div className="sticky top-0 z-40 mx-auto mb-6 flex max-w-2xl justify-between bg-secondary/95 pb-2 pt-4 backdrop-blur transition-colors duration-500 sm:px-2">
+        <Skeleton className="h-10 w-10 rounded-lg" />
+        <Skeleton className="h-10 w-10 rounded-lg" />
+      </div>
+
+      <article className="mx-auto flex max-w-2xl flex-col gap-y-4 px-1">
+        <Skeleton className="mb-4 h-10 w-3/4" />
 
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
-        <Skeleton className="h-6 w-full mb-4" />
+        <Skeleton className="mb-4 h-6 w-full" />
 
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
-        <Skeleton className="h-6 w-full mb-4" />
+        <Skeleton className="mb-4 h-6 w-full" />
 
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
-        <Skeleton className="h-6 w-full mb-4" />
+        <Skeleton className="mb-4 h-6 w-full" />
 
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
         <Skeleton className="h-6 w-full" />
-        <Skeleton className="h-6 w-full mb-4" />
-    </article>
+        <Skeleton className="mb-4 h-6 w-full" />
+      </article>
+    </>
   );
 }
