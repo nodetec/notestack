5256cc4b045ece715a72e996f464724bc9d64b3f
Christian Chiarulli <chris.machine@pm.me>
2024-08-31 05:24:10 -0400

    parsing mdast > lexical ast

---
+++ src/app/(site)/(editor)/write/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { CodeNode } from "@lexical/code";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { $convertFromMarkdownString, TRANSFORMERS } from "@lexical/markdown";
+import {
+  LexicalComposer,
+  type InitialConfigType,
+} from "@lexical/react/LexicalComposer";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { HorizontalRuleNode } from "@lexical/react/LexicalHorizontalRuleNode";
+// import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { $convertFromMarkdown } from "~/lib/lexical-md";
+import { $getRoot, $getSelection, type EditorState } from "lexical";
+import { type Link, type Root } from "mdast";
+import { remark } from "remark";
+
+const markdownToMdast = (markdown: string) => {
+  const mdast = remark().parse(markdown);
+  return mdast;
+};
+
+// When the editor changes, you can get notified via the
+// LexicalOnChangePlugin!
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+    const root = $getRoot();
+    const selection = $getSelection();
+
+    console.log("ROOT", root, selection);
+  });
+}
+
+// Lexical React plugins are React components, which makes them
+// highly composable. Furthermore, you can lazy load plugins if
+// desired, so you don't pay the cost for plugins until you
+// actually use them.
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error: unknown) {
+  console.error(error);
+}
+
+export default function Editor() {
+  const initialConfig: InitialConfigType = {
+    namespace: "NoteStackEditor",
+    // theme: getTheme("dark"),
+    // editorState: () => $convertFromMarkdownString("# ", TRANSFORMERS),
+    editorState: () => $convertFromMarkdown("#"),
+    nodes: [
+      HorizontalRuleNode,
+      //   // BannerNode,
+      HeadingNode,
+      // ImageNode,
+      // QuoteNode,
+      // CodeNode,
+      // ListNode,
+      // ListItemNode,
+      // LinkNode,
+      // AutoLinkNode,
+    ],
+    onError,
+  };
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <LexicalComposer initialConfig={initialConfig}>
+        <RichTextPlugin
+          contentEditable={
+            <ContentEditable className="prose h-full w-full max-w-none overflow-auto border border-red-500 p-12 caret-foreground dark:prose-invert selection:bg-blue-300/25 focus:outline-none" />
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+
+        <OnChangePlugin onChange={onChange} />
+        {/* <MarkdownShortcutPlugin transformers={TRANSFORMERS} /> */}
+
+        <HistoryPlugin />
+        <MyCustomAutoFocusPlugin />
+      </LexicalComposer>
+    </div>
+  );
+}
