3a3cf8c4494404a2c53b7f5bce26a883101ba771
Christian Chiarulli <chris.machine@pm.me>
2026-01-21 04:08:28 -0500

    remove reference to old plugin

--- src/components/editor/index.ts
+++ src/components/editor/index.ts
@@ -2,6 +2,5 @@ export { default as NostrEditor } from './NostrEditor';
 export type { NostrEditorHandle, HighlightSource, Highlight } from './NostrEditor';
 export type { NostrProfile, ProfileLookupFn, NostrNote, NoteLookupFn } from './context/EditorContext';
 export { default as ToolbarPlugin } from './plugins/ToolbarPlugin';
-export { default as ClickOutsidePlugin } from './plugins/ClickOutsidePlugin';
 export { default as ScrollCenterCurrentLinePlugin } from './plugins/ScrollCenterCurrentLinePlugin';
 export { default as theme } from './themes/default';
