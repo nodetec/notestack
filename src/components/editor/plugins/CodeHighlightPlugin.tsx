b50f69c0a06263bf732db8c557d45a367b11e3d8
Christian Chiarulli <chris.machine@pm.me>
2026-01-23 07:13:01 -0500

    colored code in read mode

--- src/components/editor/plugins/CodeHighlightPlugin.tsx
+++ src/components/editor/plugins/CodeHighlightPlugin.tsx
@@ -2,13 +2,21 @@
 
 import { useEffect } from 'react';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { registerCodeHighlighting } from '@lexical/code';
+import { $nodesOfType } from 'lexical';
+import { registerCodeHighlighting, CodeNode } from '@lexical/code';
 
 export default function CodeHighlightPlugin() {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
-    return registerCodeHighlighting(editor);
+    const unregister = registerCodeHighlighting(editor);
+    editor.update(() => {
+      const codeNodes = $nodesOfType(CodeNode);
+      for (const node of codeNodes) {
+        node.getWritable();
+      }
+    });
+    return unregister;
   }, [editor]);
 
   return null;
