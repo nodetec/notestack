cefbfff0e6e3dc9ad73af73bfc50c8efb7deaa70
Christian Chiarulli <chris.machine@pm.me>
2026-01-25 10:04:00 -0500

    fix type error

--- global.d.ts
+++ global.d.ts
@@ -7,12 +7,18 @@ interface Nip04 {
   decrypt(pubkey: string, ciphertext: string): Promise<string>;
 }
 
+interface Nip44 {
+  encrypt(pubkey: string, plaintext: string): Promise<string>;
+  decrypt(pubkey: string, ciphertext: string): Promise<string>;
+}
+
 // https://github.com/nostr-protocol/nips/blob/master/07.md
 interface Nostr {
   getPublicKey(): Promise<string>;
   signEvent(event: unknown): Promise<unknown>;
   getRelays?(): Promise<Record<string, { read: boolean; write: boolean }>>;
   nip04?: Nip04;
+  nip44?: Nip44;
 }
 
 declare const nostr: Nostr | undefined;
