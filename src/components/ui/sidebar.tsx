1661a8939a4dcb45f9112046570a850796cbd6a3
Christian Chiarulli <chris.machine@pm.me>
2026-01-25 19:19:50 -0500

    fix sticky header

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -136,7 +136,7 @@ function SidebarProvider({
             } as React.CSSProperties
           }
           className={cn(
-            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full overflow-x-hidden",
+            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
             className
           )}
           {...props}
